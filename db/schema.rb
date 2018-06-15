1c2671d45ffa802f089ee49e1ba7149e165ae472
Shigeki Masuda <masuda@c-fo.com>
2018-06-15 21:29:02 +0900

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_114235) do
+ActiveRecord::Schema.define(version: 2018_06_15_122603) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2018_06_15_114235) do
     t.date "due_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
